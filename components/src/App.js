8b1cb5b4384f2c2df641168ba1ee5e785652d404
Raul Jr <68832009+Raulito1@users.noreply.github.com>
2023-08-04 19:40:05 -0500

    added stying and moved to Accordian pages file

--- components/src/App.js
+++ components/src/App.js
@@ -1,41 +1,6 @@
-import React from 'react';
-import Button from './components/Button';
-
-// Icons from react-icons
-import { GoFlame } from "react-icons/go";
-import { GoAlert } from "react-icons/go";
-import { GoThumbsup } from "react-icons/go";
+// components
 
 function App() {
-    return (
-        <div>
-            <div>
-                <Button className="mb-5" primary outline rounded>
-                    Primary
-                </Button>
-            </div>
-            <div>
-                <Button className="mb-5" secondary outline rounded>
-                    Secondary
-                </Button>
-            </div>
-            <div>
-                <Button className="mb-5" success outline rounded>
-                    <GoThumbsup/>Success
-                </Button>
-            </div>
-            <div>
-                <Button className="mb-5" warning outline rounded>
-                    <GoAlert/>Warning
-                </Button>
-            </div>
-            <div>
-                <Button className="mb-5" danger outline rounded>
-                    <GoFlame/>Danger
-                </Button>
-            </div>
-        </div>
-    );
 }
 
 export default App;
---
+++ components/src/components/Accordion.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+import { BiChevronUp } from "react-icons/bi";
+import { BiChevronDown } from "react-icons/bi";
+
+function Accordion({ items }) {
+    const [expandedIndex, setExpandedIndex] = useState(-1);
+    const handleItemClick = (index) => {
+        if (index === expandedIndex) {
+            setExpandedIndex(-1);
+        } else {
+            setExpandedIndex(index);
+        }
+    };
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+        const icon = <span className='text-3xl'>{isExpanded ? <BiChevronUp/> : <BiChevronDown/>}</span>;
+        
+        return (
+            <div key={item.id}>
+                <div className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer' onClick={() => handleItemClick(index)}>
+                    {item.label}
+                    {icon} 
+                </div>
+                {isExpanded && <div className='border-b p-5'>{item.content}</div>}
+            </div>
+        );
+    });
+
+
+    return (
+        <div className='border-x border-t rounded'>
+            {renderedItems}
+        </div>
+    );
+}
+
+export default Accordion;
