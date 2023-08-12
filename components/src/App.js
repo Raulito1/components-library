dafe121353f70d780b1e053a03cbbef0a1801464
Raul Jr <68832009+Raulito1@users.noreply.github.com>
2023-08-11 20:01:26 -0500

    Created a Route Component

--- components/src/App.js
+++ components/src/App.js
@@ -1,10 +1,23 @@
 import Link from './components/Link';
+import Route from './components/Route';
+import AccordianPage from './pages/AccordianPage';
+import DropdownPage from './pages/DropdownPage';
 
 function App() {
-    return <div>
-        <Link to="/accordian">Go To Accordian</Link>
-        <Link to="/dropdown">Go To Dropdown</Link>
-    </div>
+    return (
+        <div>
+            <Link to="/accordian">Go To Accordian</Link>
+            <Link to="/dropdown">Go To Dropdown</Link>
+            <div>
+                <Route path="/accordian">
+                    <AccordianPage />
+                </Route>
+                <Route path="/dropdown">
+                    <DropdownPage />
+                </Route>
+            </div>
+        </div>
+    )
 }
 
 export default App;
